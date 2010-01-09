6fd599f2037a353a2a26842183dc7753150128a0
Adam Lantos <adam.lantos@playma.org>
2010-01-09 14:42:38 +0100

    Cleanup readme, add plugin url.

--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ require 'http_auth_patch'
 Redmine::Plugin.register :http_auth do
   name 'HTTP Authentication plugin'
   author 'Adam Lantos'
+  url "http://github.com/AdamLantos/redmine_http_auth" if respond_to?(:url)
   description 'A plugin for doing HTTP authentication'
   version '0.1'
 end
